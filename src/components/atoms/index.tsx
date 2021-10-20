f031666d729e00077757d92621fbaa41c1ab7053
Maximilian94 <mkadenfilgo@gmail.com>
2021-10-20 20:36:09 -0300

    feat: Temperature

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -2,3 +2,4 @@ export { default as PageTitle } from './PageTitle';
 export { default as DropDownOption } from './DropDownOption';
 export { default as Switch } from './Switch';
 export { default as TemperatureUnit } from './TemperatureUnit';
+export { default as Temperature } from './Temperature';
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import './style.css';
 
 import { InputsData } from '../../components/organisms';
+import { Temperature } from '../../components/atoms';
 
 const Main: React.FC = () => {
   return (
-    <div className="App">
+    <div className="main-page">
       <InputsData />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Temperature title="3,5°C" />
     </div>
   );
 };
