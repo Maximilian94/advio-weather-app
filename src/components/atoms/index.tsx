8f11ff9077e257ea25b1c0fd63ae9e4677075146
Maximilian94 <mkadenfilgo@gmail.com>
2021-10-20 20:46:17 -0300

    feat: Weather Icon

--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -3,3 +3,4 @@ export { default as DropDownOption } from './DropDownOption';
 export { default as Switch } from './Switch';
 export { default as TemperatureUnit } from './TemperatureUnit';
 export { default as Temperature } from './Temperature';
+export { default as WeatherIcon } from './WeatherIcon';
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './style.css';
 
 import { InputsData } from '../../components/organisms';
-import { Temperature } from '../../components/atoms';
+import { Temperature, WeatherIcon } from '../../components/atoms';
 
 const Main: React.FC = () => {
   return (
     <div className="main-page">
       <InputsData />
       <Temperature title="3,5°C" />
+      <WeatherIcon />
     </div>
   );
 };
