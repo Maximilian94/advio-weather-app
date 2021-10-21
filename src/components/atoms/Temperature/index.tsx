6b2371a56c38c1668a0920c309507a2d6e192f89
Maximilian94 <mkadenfilgo@gmail.com>
2021-10-21 17:42:20 -0300

    feat: Show temperature from context

--- src/components/atoms/Temperature/index.tsx
+++ src/components/atoms/Temperature/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './style.css';
 
+import { useWeather } from '../../../context/weatherContext';
+
 interface TitleProps {
   title: number | undefined;
 }
 
 const Temperature: React.FC<TitleProps> = ({ title }) => {
-  return <span className="temperature">{title || 'Loading '}</span>;
+  const { isTempUnitCelsius } = useWeather();
+
+  const fahrenheitFormula = (kelvin: number) => {
+    return ((kelvin - 273.15) * 9) / 5 + 32;
+  };
+
+  const getTemperature = () => {
+    if (isTempUnitCelsius && title !== undefined) {
+      return `${Math.round(title - 273.15)} °C`;
+    }
+    if (title !== undefined) {
+      return `${Math.round(fahrenheitFormula(title))} °F`;
+    }
+    return undefined;
+  };
+  return <span className="temperature">{getTemperature() || 'Loading '}</span>;
 };
 
 export default Temperature;
